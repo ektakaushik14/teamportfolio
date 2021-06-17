ddf969fd8873e98b92613a30bebac1d32191d898
ekta kaushik <ektakaushik14@gmail.com>
2021-06-17 20:21:03 +0530

    team

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect } from "react";
+import Portimage from "./flowerimg.png";
 
 function App() {
+  useEffect(() => {
+    const logocircle = document.querySelector(".portcircle");
+    const overlay = document.querySelector(".overlay");
+    const portimage = document.querySelector(".portimage img");
+    logocircle.addEventListener("mouseover", () => {
+      overlay.style.opacity = 1;
+      logocircle.style.backgroundColor = "white";
+      portimage.style.opacity = 1;
+    });
+    logocircle.addEventListener("mouseleave", () => {
+      overlay.style.opacity = 0;
+      logocircle.style.backgroundColor = "transparent";
+      portimage.style.opacity = 0;
+    });
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="overlay"></div>
+      <div className="Portlogo">
+        <div className="portimage">
+          <img src={Portimage} alt="" />
+          <div className="portcircle">
+            <div>E</div>
+            <div className="logoSecondLetter">P</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
